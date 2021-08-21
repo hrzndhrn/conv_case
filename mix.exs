beb9eaef7f2a23a91609744fe5454b712b2eacd4
Marcus Kruse <esurk.sucram@gmail.com>
2021-08-21 14:27:51 +0200

    Update required elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ConvCase.MixProject do
     [
       app: :conv_case,
       version: "0.2.2",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
