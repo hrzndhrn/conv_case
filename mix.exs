f346fb681daa441a340dd06d65a3e9867d2ae589
Marcus Kruse <esurk.sucram@gmail.com>
2023-06-25 09:17:30 +0200

    Add recode

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule ConvCase.MixProject do
       {:credo, "~> 1.4", only: [:dev, :test]},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.22", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.13", only: :test}
+      {:excoveralls, "~> 0.13", only: :test},
+      {:recode, "~> 0.5", only: :dev}
     ]
   end
 
